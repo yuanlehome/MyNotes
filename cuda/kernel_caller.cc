44134c113ada433cfb863b9cfe125fc37894f5ca
yuanlehome <yuanlehome@163.com>
2023-07-12 09:45:31 +0000

    update

--- cuda/kernel_caller.cc
+++ cuda/kernel_caller.cc
@@ -4,17 +4,17 @@
 #include "kernel_caller_declare.h"
 
 int main(void) {
-  // printHeader("from printHelloWorld", std::cout);
-  // printHelloWorld();
+  printHeader("from printHelloWorld", std::cout);
+  printHelloWorld();
 
-  // printHeader("from addArray", std::cout);
-  // addArray();
+  printHeader("from addArray", std::cout);
+  addArray();
 
-  // printHeader("from deviceQuery", std::cout);
-  // deviceQuery();
+  printHeader("from deviceQuery", std::cout);
+  deviceQuery();
 
-  // printHeader("from transposeSquareMatrix", std::cout);
-  // transposeSquareMatrix();
+  printHeader("from transposeSquareMatrix", std::cout);
+  transposeSquareMatrix();
 
   printHeader("from reduceSum", std::cout);
   reduceSum();
