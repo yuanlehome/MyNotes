2c679304393552b9e21c0889efb47cc9172d0830
yuanlehome <yuanlehome@163.com>
2023-08-18 11:39:00 +0000

    update reduce sum

--- cuda/kernels/reduce/reduce_sum.cu
+++ cuda/kernels/reduce/reduce_sum.cu
@@ -134,10 +134,32 @@ __global__ void reduceSumOnGPU_V4(const DATA_TYPE* d_x,
   }
 }
 
-// 数值正确 需二次 reduce 不要求数据个数为 BLOCK_SIZE 的整数倍 不改变原数组
-// 每个 block 负责多块内存数据的 reduce 其中每个线程负责跨度为整个 grid 的内存
-// 调用 warp/block reduce function
+// V5 kernel 的计算有问题 但没想明白哪里的问题 待定!!!
+// 数值错误 不要求数据个数为 BLOCK_SIZE 的整数倍 不改变原数组
+// 每个 block 负责连续的两块内存数据的 reduce 调用 block reduce function
 __global__ void reduceSumOnGPU_V5(const DATA_TYPE* d_x,
+                                  DATA_TYPE* d_y,
+                                  const int N) {
+  const int idx = threadIdx.x + 2 * blockDim.x * blockIdx.x;
+  DATA_TYPE val = 0.0;
+  if (idx < N) {
+    val += d_x[idx];
+  }
+  if (idx + blockDim.x < N) {
+    val += d_x[idx + blockDim.x];
+    // val += d_x[idx + 2 * blockDim.x];
+    // val += d_x[idx + 3 * blockDim.x];
+  }
+  val = blockReduceSum(val);
+  if (threadIdx.x == 0) {
+    atomicAdd(d_y, val);
+  }
+}
+
+// 数值正确 需二次 reduce 不要求数据个数为 BLOCK_SIZE 的整数倍 不改变原数组
+// 每个 block 负责不连续的多块内存数据的 reduce 其中每个线程负责跨度为整个 grid
+// 的内存 调用 warp/block reduce function
+__global__ void reduceSumOnGPU_V6(const DATA_TYPE* d_x,
                                   DATA_TYPE* d_y,
                                   const int N) {
   DATA_TYPE val = 0.0;
@@ -156,10 +178,10 @@ __global__ void reduceSumOnGPU_V5(const DATA_TYPE* d_x,
 
 void reduceSum() {
   constexpr uint32_t N = 1e8;
-  constexpr uint32_t M = sizeof(DATA_TYPE) * N;
+  constexpr uint32_t SIZE = sizeof(DATA_TYPE) * N;
 
   MallocWrapper cpu_allocator;
-  DATA_TYPE* h_x = (DATA_TYPE*)cpu_allocator.allocate(M);
+  DATA_TYPE* h_x = (DATA_TYPE*)cpu_allocator.allocate(SIZE);
   std::fill_n(h_x, N, a);
 
   // Timer cpu_timer;
@@ -202,8 +224,8 @@ void reduceSum() {
   dim3 grid(grid_size);
 
   GPUMallocWrapper gpu_allocator;
-  DATA_TYPE* d_x = (DATA_TYPE*)gpu_allocator.allocate(M);
-  CUDA_CHECK(cudaMemcpy(d_x, h_x, M, cudaMemcpyHostToDevice));
+  DATA_TYPE* d_x = (DATA_TYPE*)gpu_allocator.allocate(SIZE);
+  CUDA_CHECK(cudaMemcpy(d_x, h_x, SIZE, cudaMemcpyHostToDevice));
 
   DATA_TYPE* d_y = (DATA_TYPE*)gpu_allocator.allocate(sizeof(DATA_TYPE));
 
@@ -217,7 +239,8 @@ void reduceSum() {
     reduceSumOnGPU_V1<<<grid, block>>>(d_x, d_y);
     gpu_timer.stop();
     total_time += gpu_timer.elapsedTime();
-    CUDA_CHECK(cudaMemcpy(d_x, h_x, M, cudaMemcpyHostToDevice));  // 恢复原数组
+    CUDA_CHECK(
+        cudaMemcpy(d_x, h_x, SIZE, cudaMemcpyHostToDevice));  // 恢复原数组
   }
   dbg(total_time, gpu_timer.totalTime());
   CUDA_CHECK(cudaMemcpy(&y, d_y, sizeof(DATA_TYPE), cudaMemcpyDeviceToHost));
@@ -280,16 +303,34 @@ void reduceSum() {
     CUDA_CHECK(cudaMemcpy(
         d_y, &y, sizeof(DATA_TYPE), cudaMemcpyHostToDevice));  // 清空结果
     gpu_timer.start();
-    // 注意这里传的 d_x 和 d_y 相同 如果 block 之间没有同步的话 会有问题
-    reduceSumOnGPU_V5<<<10240, block_size>>>(d_x, d_x, N);
-    reduceSumOnGPU_V5<<<1, 1024>>>(d_x, d_y, 10240);
+    // 注意这里 grid 大小减半 但要向上取整
+    reduceSumOnGPU_V5<<<(grid.x + 1) / 2, block>>>(d_x, d_y, N);
     gpu_timer.stop();
     total_time += gpu_timer.elapsedTime();
-    CUDA_CHECK(cudaMemcpy(d_x, h_x, M, cudaMemcpyHostToDevice));  // 恢复原数组
   }
   dbg(total_time, gpu_timer.totalTime());
   CUDA_CHECK(cudaMemcpy(&y, d_y, sizeof(DATA_TYPE), cudaMemcpyDeviceToHost));
   dbg(y);
-  std::printf("reduceSumOnGPU_V5 (提高线程利用率) cost time: %f ms\n",
+  std::printf("reduceSumOnGPU_V5 (连续两块) cost time: %f ms\n",
+              total_time / repeats);
+
+  y = 0.0;
+  total_time = 0.0;
+  for (size_t i = 0; i < repeats; i++) {
+    CUDA_CHECK(cudaMemcpy(
+        d_y, &y, sizeof(DATA_TYPE), cudaMemcpyHostToDevice));  // 清空结果
+    gpu_timer.start();
+    // 注意这里传的 d_x 和 d_y 相同 如果 block 之间没有同步的话 会有问题
+    reduceSumOnGPU_V6<<<10240, block_size>>>(d_x, d_x, N);
+    reduceSumOnGPU_V6<<<1, 1024>>>(d_x, d_y, 10240);
+    gpu_timer.stop();
+    total_time += gpu_timer.elapsedTime();
+    CUDA_CHECK(
+        cudaMemcpy(d_x, h_x, SIZE, cudaMemcpyHostToDevice));  // 恢复原数组
+  }
+  dbg(total_time, gpu_timer.totalTime());
+  CUDA_CHECK(cudaMemcpy(&y, d_y, sizeof(DATA_TYPE), cudaMemcpyDeviceToHost));
+  dbg(y);
+  std::printf("reduceSumOnGPU_V6 (提高线程利用率) cost time: %f ms\n",
               total_time / repeats);
 }
--- cuda/notes/command.sh
+++ cuda/notes/command.sh
@@ -13,7 +13,7 @@ dlprofviewer -b 0.0.0.0 -p 8110 [*/dlprof_dldb.sqlite]
 # 
 # nvprof
 # 
-# 查看线程束的活跃比例
+# 查看线程束的活跃比例（每个周期活跃的线程束数量的平均值与一个 SM 支持的最大线程束数量的比）
 nvprof --metrics achieved_occupancy ./executable
 # 查看全局内存吞吐量（global memory load throughput）
 nvprof --metrics gld_throughput ./executable
@@ -21,6 +21,10 @@ nvprof --metrics gld_throughput ./executable
 nvprof --metrics gld_efficiency ./executable
 # 查看每个线程束执行指令的平均数量
 nvprof --metrics inst_per_warp ./executable
+# 查看分支效率
+nvprof --metrics branch_efficiency ./executable
+# 查看分支/发散分支数量
+nvprof --events branch,divergent_branch ./executable
 # 查看全局/共享内存访问事务数量（load/store）
 nvprof  --metrics shared_load_transactions,shared_store_transactions ./executable
 nvprof  --metrics shared_load_transactions_per_request,shared_store_transactions_per_request ./executable
