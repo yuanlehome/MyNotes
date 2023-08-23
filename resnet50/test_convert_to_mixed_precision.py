9d799001b776452566e37fd233b1be02b3e93d27
yuanlehome <yuanlehome@163.com>
2023-08-23 09:57:53 +0000

    update

--- resnet50/test_convert_to_mixed_precision.py
+++ resnet50/test_convert_to_mixed_precision.py
@@ -1,17 +1,25 @@
 import os
 
-import paddle
 from paddle import inference
 
-# PlaceType, PrecisionType, convert_to_mixed_precision
-
+# inference.convert_to_mixed_precision(
+#     os.path.join("./", "inference.pdmodel"),
+#     os.path.join("./", "inference.pdiparams"),
+#     os.path.join("./", "mixed_inference.pdmodel"),
+#     os.path.join("./", "mixed_inference.pdiparams"),
+#     inference.PrecisionType.Half,
+#     inference.PlaceType.GPU,
+#     False,
+# )
 
 inference.convert_to_mixed_precision(
-    os.path.join("./resnet50", "inference.pdmodel"),
-    os.path.join("./resnet50", "inference.pdiparams"),
-    os.path.join("./resnet50", "mixed_inference.pdmodel"),
-    os.path.join("./resnet50", "mixed_inference.pdiparams"),
+    os.path.join("./", "mixed_inference.pdmodel"),
+    os.path.join("./", "mixed_inference.pdiparams"),
+    os.path.join("./", "white_mixed_inference.pdmodel"),
+    os.path.join("./", "white_mixed_inference.pdiparams"),
     inference.PrecisionType.Half,
     inference.PlaceType.GPU,
     False,
+    black_list=set(["relu", "pool2d"]),
+    white_list=set(["pool2d"]),
 )
